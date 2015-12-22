6f0ff4423c505807deef6030d167a8918aaf0505
martymcflywa <doc.brown.83@gmail.com>
2015-12-22 21:49:51 +0800

    Started defining enums

---
+++ Web/Models/Constants/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Constants
+{
+    public class Enums
+    {
+        public enum Categories
+        {
+            UIAndThirdParty = 1,
+            HOTAS,
+            LeftConsole,
+            LeftAuxConsole,
+            CenterConsole,
+            RightConsole,
+            Miscellaneous,
+            Views,
+            RadioCOMMS
+        }
+
+        public enum Countries
+        {
+            US,
+            German,
+            French
+        }
+    }
+}
