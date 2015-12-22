3eadd7cf5d80c50666df83ce2bb9d92fa051bc34
martymcflywa <doc.brown.83@gmail.com>
2015-12-22 21:49:30 +0800

    Implemented custom Callback and Scancode repositories

---
+++ Web/Models/Domain/Repositories/Implementations/CallbackRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Web.Models.Domain.Context;
+using Web.Models.Domain.Entities;
+using Web.Models.Domain.Repositories.Interfaces;
+
+namespace Web.Models.Domain.Repositories.Implementations
+{
+    public class CallbackRepository : Repository<Callback>, ICallbackRepository
+    {
+        public CallbackRepository(ResourcesContext context) : base(context)
+        {
+        }
+
+        public ResourcesContext ResourcesContext => Context as ResourcesContext;
+
+        public IEnumerable<Callback> GetByCategory(int id)
+        {
+            return ResourcesContext.Callbacks
+                .Include(c => c.Category)
+                .Include(c => c.Section)
+                .Where(c => c.CategoryId == id);
+        }
+
+        public IEnumerable<Callback> GetBySection(int id)
+        {
+            return ResourcesContext.Callbacks
+                .Include(c => c.Category)
+                .Include(c => c.Section)
+                .Where(c => c.SectionId == id);
+        }
+    }
+}
---
+++ Web/Models/Domain/Repositories/Implementations/ScancodeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Web.Models.Domain.Context;
+using Web.Models.Domain.Entities;
+using Web.Models.Domain.Repositories.Interfaces;
+
+namespace Web.Models.Domain.Repositories.Implementations
+{
+    public class ScancodeRepository : Repository<Scancode>, IScancodeRepository
+    {
+        public ScancodeRepository(DbContext context) : base(context)
+        {
+        }
+
+        public ResourcesContext ResourcesContext => Context as ResourcesContext;
+
+        public IEnumerable<Scancode> GetUSCodes()
+        {
+            return ResourcesContext.Scancodes
+                .Include(c => c.USKey);
+        }
+
+        public IEnumerable<Scancode> GetGermanCodes()
+        {
+            return ResourcesContext.Scancodes
+                .Include(c => c.GermanKey);
+        }
+
+        public IEnumerable<Scancode> GetFrenchCodes()
+        {
+            return ResourcesContext.Scancodes
+                .Include(c => c.FrenchKey);
+        }
+    }
+}
