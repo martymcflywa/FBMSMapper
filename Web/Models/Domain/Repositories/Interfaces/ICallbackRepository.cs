ff4970e1353c176cbc4e084928a0bbf8c5ed6346
martymcflywa <doc.brown.83@gmail.com>
2015-12-22 20:33:39 +0800

    Added custom interfaces for Callbacks and Scancodes

---
+++ Web/Models/Domain/Repositories/Interfaces/ICallbackRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccess.Interfaces;
+using Web.Models.Domain.Entities;
+
+namespace Web.Models.Domain.Repositories.Interfaces
+{
+    public interface ICallbackRepository : IRepository<Callback>
+    {
+        IEnumerable<Callback> GetByCategory(int id);
+        IEnumerable<Callback> GetBySection(int id);
+    }
+}
---
+++ Web/Models/Domain/Repositories/Interfaces/IScancodeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Interfaces;
+using Web.Models.Domain.Entities;
+
+namespace Web.Models.Domain.Repositories.Interfaces
+{
+    public interface IScancodeRepository : IRepository<Scancode>
+    {
+        IEnumerable<Scancode> GetUsCodes();
+        IEnumerable<Scancode> GetGermanCodes();
+        IEnumerable<Scancode> GetFrenchCodes();
+    }
+}
