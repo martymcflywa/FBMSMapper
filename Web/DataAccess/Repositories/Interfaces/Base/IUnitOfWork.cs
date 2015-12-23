10f228c67bfa3ebdbb195ba19a1814ff8855a781
martymcflywa <doc.brown.83@gmail.com>
2015-12-23 13:24:59 +0800

    Implemented UnitOfWork

---
+++ Web/DataAccess/Repositories/Interfaces/Base/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.DataAccess.Repositories.Interfaces.Base
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICallbackRepository Callbacks { get; set; }
+        IScancodeRepository Scancodes { get; set; }
+        int Complete();
+    }
+}
---
+++ Web/DataAccess/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.DataAccess.Domain.Context;
+using Web.DataAccess.Repositories.Interfaces;
+using Web.DataAccess.Repositories.Interfaces.Base;
+
+namespace Web.DataAccess.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ResourcesContext _context;
+        public ICallbackRepository Callbacks { get; set; }
+        public IScancodeRepository Scancodes { get; set; }
+
+        public UnitOfWork(ResourcesContext context)
+        {
+            _context = context;
+            Callbacks = new CallbackRepository(_context);
+            Scancodes = new ScancodeRepository(_context);
+        }
+        
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
