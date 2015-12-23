cc3951a2b920096f07177242b259c45b32c47c5d
martymcflywa <doc.brown.83@gmail.com>
2015-12-23 20:29:18 +0800

    Implemented Device config and Joystick

---
+++ Web/Models/Device/DeviceConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Device
+{
+    public class DeviceConfiguration
+    {
+        protected bool IsDXShifting { get; }
+        protected int CountryId { get; set; } = (int) Constants.Countries.US;
+        protected int NumberOfDevices { get; set; } = Constants.DefaultNumberOfDevices;
+        protected int ShiftMagnitude { get; set; } = Constants.DefaultShiftMagnitude;
+
+        protected int ShiftDevices
+        {
+            get { return Constants.MaxDevices - NumberOfDevices; }
+        }
+
+        public DeviceConfiguration(bool isDXShifting, int? countryId, int? numberOfDevices, int? shiftMagnitude)
+        {
+            IsDXShifting = isDXShifting;
+
+            // if null, use defaults
+            if(countryId == null)
+            {
+                CountryId = (int)Constants.Countries.US;
+            } else
+            {
+                CountryId = (int)countryId;
+            }
+
+            if (numberOfDevices == null)
+            {
+                NumberOfDevices = Constants.DefaultNumberOfDevices;
+            } else
+            {
+                NumberOfDevices = (int)numberOfDevices;
+            }
+
+            if(shiftMagnitude == null)
+            {
+                ShiftMagnitude = Constants.DefaultShiftMagnitude;
+            } else
+            {
+                ShiftMagnitude = (int)shiftMagnitude;
+            }
+        }
+    }
+}
---
+++ Web/Models/Device/Joystick.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Device
+{
+    public class Joystick
+    {
+        private readonly DeviceConfiguration _deviceConfiguration;
+
+        public int JoystickId { get; set; }
+        public int Offset
+        {
+            get { return JoystickId * Constants.Offset; }
+        }
+
+        public Joystick(DeviceConfiguration deviceConfiguration, int joystickId)
+        {
+            _deviceConfiguration = deviceConfiguration;
+            JoystickId = joystickId;
+        }
+    }
+}
