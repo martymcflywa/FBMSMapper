bf325bbe5cb7b1f808a18852be9ba7964367c9f3
martymcflywa <doc.brown.83@gmail.com>
2015-12-24 15:09:39 +0800

    Defined keyboard assignment object

---
+++ Web/Models/Device/BaseAssignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Device
+{
+    public class BaseAssignment
+    {
+        public string Callback { get; set; }
+        public int SoundId { get; set; }
+    }
+}
---
+++ Web/Models/Device/KeyboardAssignment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models.Device
+{
+    public class KeyboardAssignment : BaseAssignment
+    {
+        public int MouseOrKeyboard { get; set; }
+        public string KeyScancode { get; set; }
+        public int KeyModifierId { get; set; }
+        public int ComboScancodeId { get; set; }
+        public int ComboModifierId { get; set; }
+        public int UIAttributeId { get; set; }
+        // get description from database where CallbackId is a match
+        public string Description { get; set; }
+
+        public KeyboardAssignment(string callback, int? soundId, string keyScancode, int keyModifierId,
+            int? comboScancodeId, int? comboModifierId, int? uiAttributeId, string description)
+        {
+            Callback = callback;
+            SoundId = soundId ?? Constants.NoSound;
+            MouseOrKeyboard = Constants.NotInUse;
+
+            KeyScancode = keyScancode;
+            KeyModifierId = keyModifierId;
+
+            // use default values if null
+            ComboScancodeId = comboScancodeId ?? Constants.NotInUse;
+            ComboModifierId = comboModifierId ?? Constants.NotInUse;
+            UIAttributeId = uiAttributeId ?? Constants.Visible;
+
+            Description = description;
+        }
+    }
+}
