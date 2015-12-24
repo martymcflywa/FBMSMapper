e7c7aca705f309a6104907ab9f0ad52fbea56ac6
martymcflywa <doc.brown.83@gmail.com>
2015-12-24 18:09:27 +0800

    Started Mapping interface

---
+++ Web/Controllers/Mapping/IKeyboardMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Controllers.Mapping
+{
+    public interface IKeyboardMapper : IMapper
+    {
+        void AddAssignment(string callback, int? soundId, string keyScancode, int keyModifierId, string comboScancode, int? comboModifierId, int? uiAttributeId, string description);
+    }
+}
---
+++ Web/Controllers/Mapping/IMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.DataAccess.Repositories;
+using Web.DataAccess.Repositories.Interfaces.Base;
+
+namespace Web.Controllers.Mapping
+{
+    public interface IMapper
+    {
+        UnitOfWork UnitOfWork { get; set; }
+        List<string> AssignmentList { get; set; }
+        List<string> GetAssignments();
+        void RemoveAssignment();
+    }
+}
